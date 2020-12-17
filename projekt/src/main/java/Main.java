aae546b3e8271b6b68138b38aacf933c761e8596
piotrekpuszczynski <piotrekpuszcz@icloud.com>
2020-12-17 21:23:57 +0100

    dodanie menu

--- projekt/src/main/java/Main.java
+++ projekt/src/main/java/Main.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
-import menu.*;
+import frames.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame test = new Window();
         test.setVisible(true);
         test.setLocationRelativeTo(null);
+
     }
 }
--- projekt/src/main/java/frames/Window.java
+++ projekt/src/main/java/frames/Window.java
@@ -1,4 +1,4 @@
-package menu;
+package frames;
 
 import javax.swing.*;
 import java.awt.*;
